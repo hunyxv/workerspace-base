0479f4dfaa787c9b3d28c670a5578650a1cfbe89
wanghongning <wanghongning@xin.com>
2020-09-22 13:50:01 +0800

    start env

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ then
 
 cat>>.bashrc<<EOF
 export GOROOT=/usr/local/lib/go
-export GOPATH=/root/Projects/go
+export GOPATH=/root/go
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 export GO111MODULE=on
 export GOPROXY=https://goproxy.io
